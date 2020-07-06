13cf05cdcfebe48cbc2bf8aa17b16816496829a0
1686697639@qq.com <1686697639@qq.com>
2020-07-06 09:21:12 +0800

    有bug的swiper

---
+++ swiper/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <main-swiper></main-swiper>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import mainSwiper from '@/components/mainSwiper/mainSwiper'
+export default {
+  components: {
+    mainSwiper
+  }
+}
+</script>
+
+<style>
+@import url('./assets/css/base.css');
+</style>
---
+++ swiper/src/components/mainSwiper/mainSwiper.vue
@@ -0,0 +1,43 @@
+<!--  -->
+<template>
+  <swiper>
+    <swiper-item v-for="item in imgs" :key="item.id">
+      <img :src="item" alt="" @load="imageLoad">
+    </swiper-item>
+    <!-- <swiper-item>
+      <img src="@/assets/img/swiper1.jpg" alt="">
+    </swiper-item> -->
+  </swiper>
+</template>
+
+<script>
+import Swiper from '../swiper/Swiper';
+import SwiperItem from '../swiper/SwiperItem';
+export default {
+  name: "mainSwiper",
+  data() {
+    return {
+      isLoad:false,
+      imgs: [
+        require('@/assets/img/swiper1.jpg'),
+        require('@/assets/img/swiper2.jpg'),
+        require('@/assets/img/swiper3.jpg')
+      ]
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  methods: {
+    imageLoad() {
+      if(!this.isLoad) {
+        this.$emit('swiperImageLoad')
+        this.isLoad = true
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
